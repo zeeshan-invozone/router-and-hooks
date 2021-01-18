d7cc64619f7aeefbcf1d92f4d5003b071fa91d76
zeeshan_akram <zeeshan.akram@invozone.com>
2021-01-18 15:10:58 +0500

    configure eslint prettier and husky

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    node: true,
+  },
+  plugins: ['react'],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+      experimentalObjectRestSpread: true,
+    },
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-eval': 'error',
+  },
+}
